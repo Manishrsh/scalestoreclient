a6e3ff215363eb038c3d9a3a87c0b80b5034312c
Manishrsh <manishrs5235@gmail.com>
2024-09-05 00:21:25 +0530

    ready for test in cloud

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Load environment variables
-const backendApiUrl = process.env.VITE_BACKEND_API;
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy API requests to your backend server using the environment variable
+      // Proxy API requests to your backend server
       '/api': {
-        target: backendApiUrl,
+        target: 'http://manishsofbackend.scalestore.shop',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
