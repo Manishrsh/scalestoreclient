91c25b4dc9b6c5a800628de260b8583f0b57edce
Manishrsh <manishrs5235@gmail.com>
2024-09-04 23:54:52 +0530

    ready for test in cloud

--- src/componets/Check.jsx
+++ src/componets/Check.jsx
@@ -2,16 +2,21 @@ import React, { useEffect } from 'react'
 import axios from 'axios'
 
 const Check = () => {
-    useEffect(()=>{
-         const getdata = async()=>{
-            const res = await axios.get(`${import.meta.env.VITE_BACKEND_API}/`)
-            console.log(res)
-         }
-         getdata()
-    },[])
-  return (
-    <div>Check</div>
-  )
+    useEffect(() => {
+        const getdata = async () => {
+            try {
+                const res = await axios.get(`${import.meta.env.VITE_BACKEND_API}/`)
+                console.log(res.data) // Use res.data to get the response body
+            } catch (error) {
+                console.error("Error fetching data:", error)
+            }
+        }
+        getdata()
+    }, [])
+
+    return (
+        <div>Check</div>
+    )
 }
 
 export default Check
