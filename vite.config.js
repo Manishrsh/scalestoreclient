1daffcd5bed2532802c910bc8a06074ace8243af
Manishrsh <manishrs5235@gmail.com>
2024-09-05 00:11:16 +0530

    ready for test in cloud

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Load environment variables
+const backendApiUrl = process.env.VITE_BACKEND_API;
+
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Proxy API requests to your backend server using the environment variable
+      '/api': {
+        target: backendApiUrl,
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
