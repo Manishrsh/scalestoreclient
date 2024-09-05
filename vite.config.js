b2f1c77c97d5141bae250a99626d4f2060fb6155
Manishrsh <manishrs5235@gmail.com>
2024-09-05 09:17:31 +0530

    ready for test in cloud

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      // Proxy API requests to your backend server
       '/api': {
         target: 'http://manishsofbackend.scalestore.shop',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        // Remove rewrite if your backend API has /api in the URL
+        // rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
