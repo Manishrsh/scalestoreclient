472c8c536e169c182add8dfd5648aec6f03036b6
Manishrsh <manishrs5235@gmail.com>
2024-09-09 23:44:44 +0530

    frist add

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://manishsofbackend.scalestore.shop',
-        changeOrigin: true,
-        // Remove rewrite if your backend API has /api in the URL
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
 })
